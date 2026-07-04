871fb6f6757df0992812d2579e1af10160307dd8
Bkuzhali <kuzhalibakki59@gmail.com>
2026-07-04 14:32:11 +0000

    Pending changes exported from your codespace

--- spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -2,27 +2,11 @@ package com.cognizant.spring_learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @SpringBootApplication
 public class SpringLearnApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SpringLearnApplication.class, args);
-        System.out.println("SpringLearnApplication started successfully!");
-        displayDate();  // call the method here
-    }
-
-    public static void displayDate() {
-        ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
-        SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
-        try {
-            Date date = format.parse("31/12/2018");
-            System.out.println("Parsed Date: " + date);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
--- spring-learn/src/main/java/com/cognizant/spring_learn/controller/HelloController.java
+++ spring-learn/src/main/java/com/cognizant/spring_learn/controller/HelloController.java
@@ -1,12 +1,18 @@
-package com.cognizant.spring_learn;
+package com.cognizant.spring_learn.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World!!";
+    }
+
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from Spring Boot!";
+        return "Hello World!!";
     }
 }
